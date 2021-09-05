1dc59838eb7107521adef16ee29f7b49787fffc3
DuongHoDu <dub1706570@student.ctu.edu.vn>
2021-09-06 01:36:56 +0700

    update 5/9/2021

--- src/component/Paypal.js
+++ src/component/Paypal.js
@@ -22,14 +22,14 @@ export default class Paypal extends React.Component {
             // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
         }
  
-        let env = 'sandbox'; // you can set here to 'production' for production
+        //let env = 'sandbox'; // you can set here to 'production' for production
         let currency = 'USD'; // or you can set this value from your props or state
         let total = this.props.total; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
  
         const client = {
-            sandbox:    this.props.client,
-            production: 'PRODUCT-ID',
+            sandbox: this.props.client,
+            production: this.props.client,
         }
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first
         // For sandbox app-ID (after logging into your developer account, please locate the "REST API apps" section, click "Create App"):
@@ -39,7 +39,7 @@ export default class Paypal extends React.Component {
  
         // NB. You can also have many Paypal express checkout buttons on page, just pass in the correct amount and they will work!
         return (
-            <PaypalExpressBtn env={env} client={client} currency={currency} total={total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} style={{size: 'responsive',}} />
+            <PaypalExpressBtn  client={client} currency={currency} total={total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} style={{size: 'responsive',}} />
         );
     }
 }
--- src/pages/RoomBooking.js
+++ src/pages/RoomBooking.js
@@ -4,9 +4,10 @@ import publicAPI from '../api/publicAPI';
 import Paypal from '../component/Paypal';
 import Footer from '../component/Footer';
 import logo from '../img/logo.png';
+import { Link } from 'react-router-dom';
 
 export default class RoomBooking extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             host_id: "",
@@ -21,55 +22,65 @@ export default class RoomBooking extends Component {
             room_type: "",
             room_capacity: "",
             host_paypal_id: "",
+            host_number_bank: "",
+            host_branch_name: "",
+            host_owner_account: "",
             member_id: "",
             number_night: 0,
             available: true,
+            class_noti_payment: "noti-payment-hidden",
+            class_payment_way: "payment-way-hidden",
+            paypal: true,
         }
         this.DateChange = this.DateChange.bind(this);
     }
 
-    componentDidMount(){
+
+    componentDidMount() {
         let search = queryString.parse(this.props.location.search);
         let params = new FormData();
         params.append("id_room", search.roomID);
         const api = new publicAPI();
         api.getRoomByID(params)
-            .then(response =>{
+            .then(response => {
                 this.setState({
                     checkin_date: search.check_in,
                     checkout_date: search.check_out,
                     host_id: search.hostID,
                     room_id: search.roomID,
                     room_price: response[0].price_room,
-                    price_USD: response[0].price_room / 22000,
+                    price_USD: response[0].price_room / 22400,
                     room_name: response[0].name_room,
                     room_type: response[0].type_room,
                     room_capacity: response[0].capacity_room,
                     member_id: localStorage.getItem("iduser"),
                 })
             })
-            .catch(error =>{
+            .catch(error => {
                 console.log(error);
             })
-        
+
         let params1 = new FormData();
         params1.append("id", search.hostID);
         api.gethostbyID(params1)
-            .then(response =>{
+            .then(response => {
                 this.setState({
                     host_name: response.company_name,
                     host_address: response.address_host,
                     host_paypal_id: response.paypal_id,
+                    host_number_bank: response.bank_number,
+                    host_branch_name: response.branch_name,
+                    host_owner_account: response.owner_acount,
                 })
             })
-            .catch(error =>{
+            .catch(error => {
                 console.log(error);
             })
 
-        if(search.check_in !== "" && search.check_out !== ""){
+        if (search.check_in !== "" && search.check_out !== "") {
             var date_start = Date.parse(search.check_in);
             var date_end = Date.parse(search.check_out);
-            this.setState({ 
+            this.setState({
                 number_night: (date_end - date_start) / 86400000
             });
         }
@@ -77,7 +88,8 @@ export default class RoomBooking extends Component {
         this.CheckRoomAvailable();
     }
 
-    CheckRoomAvailable(){
+
+    CheckRoomAvailable() {
         let search = queryString.parse(this.props.location.search);
         let params = new FormData();
         params.append("checkin", search.check_in);
@@ -86,7 +98,7 @@ export default class RoomBooking extends Component {
         api.getRoomUnavailable(params)
             .then(response => {
                 var listRoomUnavailable = response;
-                if(listRoomUnavailable.indexOf(search.roomID) !== -1 || search.check_in === "" || search.check_out === ""){
+                if (listRoomUnavailable.indexOf(search.roomID) !== -1 || search.check_in === "" || search.check_out === "") {
                     this.setState({ available: false });
                 }
             })
@@ -95,28 +107,59 @@ export default class RoomBooking extends Component {
             })
     }
 
-    DateChange = (e) =>{
-        if(e.target.name === "checkin_date"){
+    DateChange = (e) => {
+        if (e.target.name === "checkin_date") {
             this.props.history.push("/rooms/book?roomID=" + this.state.room_id + "&hostID=" + this.state.host_id + "&check_in=" + e.target.value + "&check_out=" + this.state.checkout_date)
         }
-        if(e.target.name === "checkout_date"){
+        if (e.target.name === "checkout_date") {
             this.props.history.push("/rooms/book?roomID=" + this.state.room_id + "&hostID=" + this.state.host_id + "&check_in=" + this.state.checkin_date + "&check_out=" + e.target.value)
         }
         window.location.reload();
     }
-    
+
+    RoomNotAvailableNoti = () => {
+        document.getElementById("notification-payment").innerHTML = "Phòng hiện không khả dụng. Vui lòng chọn thời gian khác hoặc phòng khác!";
+        this.setState({
+            class_noti_payment: "noti-payment-visible",
+        })
+    }
+
+    ChoosePaymentWay = () =>{
+        document.getElementById("notification-payment").innerHTML = "Vui lòng kiểm tra kĩ các thông tin về phòng trước khi chuyển khoản thanh toán!";
+        this.setState({
+            class_noti_payment: "noti-payment-visible",
+            class_payment_way: "payment-way-visible",
+        })
+    }
+
+    HiddenNoti = () => {
+        this.setState({ class_noti_payment: "noti-payment-hidden" });
+    }
+
+
+    ChangePaymentWay = (way) =>{
+        if(way === "paypal"){
+            this.setState({ paypal: true });
+        }
+        else{
+            this.setState({ paypal: false });
+        }
+    }
+
 
     render() {
         var priceFormat = new Intl.NumberFormat();
+        var deposit = parseFloat(this.state.price_USD * this.state.number_night * 0.1).toFixed(2);
+        deposit = parseFloat(deposit);
         return (
-            <div>
+            <div key={1}>
                 <div className="col-md-12 header-book-room">
                     <img src={logo} alt="logo-web" />
                 </div>
                 <div className="col-md-8 col-md-offset-2 body-book-room">
                     <div className="book-room-div-left">
                         <div className="col-md-12">
-                            <span className="glyphicon glyphicon-chevron-left"></span>
+                            <Link to={"/rooms/" + this.state.host_id + "?check_in=" + this.state.checkin_date  + "&check_out=" + this.state.checkout_date}><span className="glyphicon glyphicon-chevron-left"></span></Link>
                             <label className="title-large">Xác nhận đặt phòng và thanh toán</label>
                         </div>
                         <div className="col-md-12">
@@ -150,6 +193,8 @@ export default class RoomBooking extends Component {
                             <p>Khi xác nhận đặt phòng bạn sẽ chọn 1 trong 2 phương thức thanh toán MoMo hoặc Paypal để đặt cọc và hoàn tất việc thanh toán.</p>
                             <p>Sau khi đặt phòng bạn nên theo dõi lịch sử để theo dõi lịch đặt đã được xác nhận hay chưa.</p>
                             <p>Trong trường hợp bạn hủy phòng thì tiền cọc sẽ không được hoàn lại. Mọi ngoại lệ bạn nên liên lạc trực tiếp với chủ khách sạn.</p>
+                            <p>Khi bạn chọn phương thức PAYPAL để thanh toán bạn sẽ được xác nhận đặt phòng ngay lập tức</p>
+                            <p>Khi bạn chọn phương thức CHUYỂN KHOẢN NGÂN HÀNG để thanh toán bạn sẽ phải đợi 15 - 30 phút để xác nhận</p>
                         </div>
                     </div>
                     <div className="book-room-div-right">
@@ -169,20 +214,51 @@ export default class RoomBooking extends Component {
                             <p className="col-md-7">Tổng cộng:</p>
                             <p className="col-md-5">{priceFormat.format(this.state.room_price * this.state.number_night)} (VND)</p>
                             <p className="col-md-5 col-md-offset-7">~{priceFormat.format(this.state.price_USD * this.state.number_night)} (USD)</p>
+                            <p className="col-md-7">Đặt cọc (10%):</p>
+                            <p className="col-md-5">{priceFormat.format(this.state.room_price * this.state.number_night * 0.1)} (VND)</p>
+                            <p className="col-md-5 col-md-offset-7">~{priceFormat.format(this.state.price_USD * this.state.number_night * 0.1)} (USD)</p>
                         </div>
                         <div className="col-md-12 paypal-btn">
                             {
                                 this.state.available ? (
-                                    <Paypal client={this.state.host_paypal_id} total={parseFloat(this.state.price_USD * this.state.number_night).toFixed(2)}/>
+                                    <button className="payment-available-btn" onClick={this.ChoosePaymentWay}>ĐẶT PHÒNG</button>
                                 ) : (
-                                    <button className="payment-not-available-btn"></button>
+                                    <button className="payment-not-available-btn" onClick={this.RoomNotAvailableNoti}>ĐẶT PHÒNG</button>
                                 )
                             }
+                            <div className={this.state.class_payment_way}>
+                                <label>Chọn phương thức thanh toán</label>
+                                <span><input type="radio" name="optradio" onClick={this.ChangePaymentWay.bind(this, "paypal")} /> Thanh toán qua Paypal</span>
+                                <span><input type="radio" name="optradio" onClick={this.ChangePaymentWay.bind(this, "bank")} /> Thanh toán qua chuyển khoản ngân hàng</span>
+                                <div className="payment-way">
+                                    {
+                                        this.state.paypal ? (
+                                            <Paypal client={this.state.host_paypal_id} total={deposit}/>
+                                        ) : (
+                                            <div>
+                                                <p><label>Số tài khoản:</label> {this.state.host_number_bank}</p>
+                                                <p><label>Ngân hàng:</label> {this.state.host_branch_name}</p>
+                                                <p><label>Chủ tài khoản:</label> {this.state.host_owner_account}</p>
+                                                <p><label>Số tiền cần chuyển:</label> {priceFormat.format(this.state.room_price * this.state.number_night * 0.1)} (VND)</p>
+                                                <button className="btn btn-warning">Xác nhận</button>
+                                            </div>
+                                        )
+                                    }
+                                    
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-md-12">
                     <Footer />
+                </div>
+                <div className={this.state.class_noti_payment + " col-md-12"}>
+                    <div>
+                        <p id="notification-payment"></p>
+                        <button className="btn btn-danger" onClick={this.HiddenNoti}>OK</button>
+                    </div>
+
                 </div>
             </div>
         )
