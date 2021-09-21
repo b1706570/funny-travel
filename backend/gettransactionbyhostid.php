11f50bfcce141951e62ac2abffc09fe2b94b45c9
DuongHoDu <dub1706570@student.ctu.edu.vn>
2021-09-22 01:09:38 +0700

    update 21/9/2021

--- backend/gettransactionbyhostid.php
+++ backend/gettransactionbyhostid.php
@@ -1,7 +1,7 @@
 <?php
     include 'default.php';
     $response = [];
-    $sql = "SELECT * FROM `transaction` WHERE `id_room` IN (SELECT `id_room` FROM `rooms` WHERE `id_host`=".$_POST['id_host'].")";
+    $sql = "SELECT t.*, m.`fullname`, r.`name_room` FROM `transaction` t JOIN `rooms` r ON t.`id_room`=r.`id_room` JOIN `member` m ON t.`id_member`=m.`id_mem` WHERE t.`id_room` IN (SELECT `id_room` FROM `rooms` WHERE `id_host`=".$_POST['id_host'].")";
     $result = $connect->query($sql);
     while ($row = $result->fetch_assoc()) {
         $response[] = $row;
--- src/pages/HostManage.js
+++ src/pages/HostManage.js
@@ -4,33 +4,104 @@ import hostAPI from '../api/hostAPI';
 import { Redirect } from 'react-router';
 
 export default class HostManage extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             TotalRevenue: 0,
             AllTransaction: [],
-            RevenuePerMonth: [],
-            StatisticsEachRoom: [],
-            CancellationRate: [],
+            StatisticsRevenue: {},
+            StatisticsEachRoom: {},
+            CancellationRate: {},
         }
+        this.StatisticsRevenue = this.StatisticsRevenue.bind(this);
+        this.TotalRevenue = this.TotalRevenue.bind(this);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         let params = new FormData();
         params.append("id_host", localStorage.getItem("iduser"));
         const api = new hostAPI();
         api.getTransactionByOfHost(params)
-            .then(response =>{
+            .then(response => {
+                var allTrans = response;
                 console.log(response);
+                this.StatisticsRevenue(allTrans, "day");
+                this.TotalRevenue(allTrans, "today");
+                this.StatisticsEachRoom(allTrans);
+                this.setState({ AllTransaction: response });
             })
-            .catch(error =>{
+            .catch(error => {
                 console.log(error);
             })
+    }
+
+    TotalRevenue = (alldata, condition) => {
+        if (condition === "today") {
+            let now = new Date();
+            let today = now.getFullYear() + "-" + ("0" + (now.getMonth() + 1)).slice(-2) + "-" + ("0" + now.getDate()).slice(-2) + " 12:00:00";
+            let value = 0;
+            for (let i = 0; i < alldata.length; i++) {
+                if (alldata[i].checkout_date === today)
+                    value += alldata[i].total_payment;
+            }
+            this.setState({ TotalRevenue: value });
+        }
+    }
+
+    StatisticsRevenue = (alldata, condition) => {
+        if (condition === "day") {
+            let now = new Date();
+            let value = [];
+            let label = [];
+            for (let i = 0; i < now.getDate(); i++) {
+                label[i] = (i + 1) + "/" + (now.getMonth() + 1);
+                value[i] = 0;
+                for (let x = 0; x < alldata.length; x++) {
+                    if (alldata[x].checkout_date === now.getFullYear() + "-" + ("0" + (now.getMonth() + 1)).slice(-2) + "-" + ("0" + (i + 1)).slice(-2) + " 12:00:00")
+                        value[i] += Number(alldata[x].total_payment);
+                }
+            }
+            let revenue = {};
+            revenue['label'] = label;
+            revenue['value'] = value;
+            this.setState({ StatisticsRevenue: revenue });
+        }
+    }
+
+    StatisticsEachRoom = (alldata) => {
+        alldata.sort((a, b) => (a.name_room > b.name_room) ? 1 : -1);
+        let label = [];
+        let value = [];
+        alldata.forEach((img) => {
+            if (label.indexOf(img.name_room) === -1) {
+                label.push(img.name_room);
+                value.push(1);
+            }
+            else{
+                value[label.indexOf(img.name_room)] += 1;
+            }
+        });
+        let data = {};
+        data['label'] = label;
+        data['value'] = value;
+        this.setState({ StatisticsEachRoom: data });
     }
 
     render() {
         if (localStorage.getItem("type") !== 'host') {
             return <Redirect to="/" />
+        }
+        if (this.state.AllTransaction.length === 0) {
+            return (
+                <div>
+                    <div className="host-header col-md-12">
+                        <HostHeader hostActive={2} />
+                    </div>
+                    <div className="noti-empty-booking-schedule">
+                        <p>Bạn không có lịch đặt nào cả</p>
+                    </div>
+                </div>
+            )
         }
         return (
             <div>
